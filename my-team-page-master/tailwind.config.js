7f99ab335ed2876e3eb722dde941574d78f74ca4
orjichimaprince <orjichimaprince@gmail.com>
2022-09-11 19:49:42 +0100

    mobile-first

---
+++ my-team-page-master/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // content: ["./src/public/*.{html,js}"],
+    content: ["./public/*.html"],
+    // content: ["../public/index.html"],
+    theme: {
+        extend: {
+            // container: { center: true, padding: '2rem' },
+            // columns: { '4xs': '14rem', },
+            // gridTemplateColumns: {
+            //     '16': 'repeat(16, minmax(0, 1fr))',
+            //     'footer': '200px minmax(900px, 1fr) 100px',
+            // },
+        }
+    },
+    plugins: [],
+}
